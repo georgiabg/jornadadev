e1c0f4d63c6d267e57d554e8528138947ae26adf
georgiabg <georgiabgomes@hotmail.com>
2023-03-31 16:36:37 -0300

    Trabalhando no Footer

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import VideoFooter from "./components/footer/VideoFooter";
 import "./video.css";
 
 function Video() {
@@ -24,6 +25,8 @@ function Video() {
         loop
         src="https://firebasestorage.googleapis.com/v0/b/jornada3-a7924.appspot.com/o/brecker2.mp4?alt=media&token=5493f40a-3b0d-4c19-b5dd-a9016cc5a6d1"
       ></video>
+      {/* Side bar */}
+      <VideoFooter />
     </div>
   );
 }
